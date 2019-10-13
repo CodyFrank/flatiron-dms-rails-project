f11bdbc52d9c4bc10f46bedcf5c67bd86fab2bce
Cody <cody.frank30@gmail.com>
2019-10-13 19:42:04 -0400

    destroyed resource user

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-    has_many :repair_orders
-    has_many :vehicles, through: :repair_orders
-    has_many :jobs, through: :repair_orders
-    has_secure_password
-end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   resources :jobs
   resources :repair_orders
   resources :vehicles
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
