e33b0700a2c5ba04c00725d4d0b382727cd32b51
Cody <cody.frank30@gmail.com>
2019-10-14 21:28:04 -0400

    added employee validations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,4 +4,10 @@ class Employee < ApplicationRecord
     has_many :jobs, through: :repair_orders
     has_many :customers, through: :repair_orders
     has_secure_password
+
+    validates :name, :worker_id, :job_title, presence: true
+    validates :worker_id, uniqueness: true
+    validates :password, confirmation: true
+    validates :password_confirmation, presence: true
+    validates :worker_id, length: { is: 4 }
 end
