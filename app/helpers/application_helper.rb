bbae61febe8915e52c2fc2f48649a38f17e2fe44
Cody <cody.frank30@gmail.com>
2019-10-16 19:36:28 -0400

    added navbar helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def navbar
+        if logged_in?
+            render "layouts/logged_in_navbar"
+        else
+            render "layouts/logged_out_navbar"
+        end
+    end
+    
 end
