b5e8b12c4f30a8e67ec9e580e8bb063674d031fa
Cody <cody.frank30@gmail.com>
2019-10-16 19:48:45 -0400

    created nav bar views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
+    helper_method :logged_in?
+    helper_method :current_user
+
     def home
+    end
+
+    private
+
+
+    def logged_in?
+        !! current_user
+    end
+
+    def employee_user
+        current.find_by(id: sessions[:user_id])
     end
 end
