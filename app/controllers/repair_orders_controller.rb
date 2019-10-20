33e0f46bfd90ee92da93c97600808db4469a70f7
Cody <cody.frank30@gmail.com>
2019-10-19 21:25:52 -0400

    created repair_order create action and repair order params method

--- app/controllers/repair_orders_controller.rb
+++ app/controllers/repair_orders_controller.rb
@@ -1,7 +1,21 @@
 class RepairOrdersController < ApplicationController
+
     def new
         @repair_order = RepairOrder.new
     end
+
     def create
+        @repair_order = RepairOrder.new(repair_order_params)
+        if @repair_order.save
+            redirect_to repair_order_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def repair_order_params
+        params.require(:repair_order).permit(:vin_number, :worker_number, :customer_email)
     end
 end
