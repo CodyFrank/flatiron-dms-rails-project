954cb94639db598e109ead730ad4a8a7fb719248
Cody <cody.frank30@gmail.com>
2019-10-26 14:52:08 -0400

    removed has_secure_password and password/pw confirmation validations from customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,13 +4,9 @@ class Customer < ApplicationRecord
     has_many :jobs, through: :repair_orders
     has_many :employees, through: :repair_orders
 
-    has_secure_password
 
     validates :name, presence: true
     validates :email, :phone_number, uniqueness: true
     validates :phone_number, length: {is: 10}
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-    validates :password, confirmation: true
-    validates :password_confirmation, presence: true, on: :create
-    
 end
