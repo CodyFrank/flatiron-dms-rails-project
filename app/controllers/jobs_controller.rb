354c82ef92bf481459cf672d21345586eb3945cf
Cody <cody.frank30@gmail.com>
2019-10-27 12:59:22 -0400

    restructure create method and add edit/update methods to jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,17 +1,30 @@
 class JobsController < ApplicationController
 
+    before_action :authenticate 
+
     def new
         @job = Job.new
     end
 
     def create
         @job.new(job_params)
+        if @job.save
+            redirect_to repair_order_path(@job.repair_order)
+        else
+            render :new
+        end
     end
 
     def edit
+        @job = Job.find(params[:id])
     end
 
     def update
+        if @job.update(job_params)
+            redirect_to repair_order_path(@job.repair_order)
+        else
+            render :edit
+        end
     end
 
     def destroy
