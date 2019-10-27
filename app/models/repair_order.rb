de62a914992fb7604f8438eef7a07f7e08999e52
Cody <cody.frank30@gmail.com>
2019-10-27 07:35:09 -0400

    added dependand destroy to vehicles, repair orders, and customer models

--- app/models/repair_order.rb
+++ app/models/repair_order.rb
@@ -2,7 +2,7 @@ class RepairOrder < ApplicationRecord
     belongs_to :employee
     belongs_to :vehicle
     belongs_to :customer
-    has_many :jobs
+    has_many :jobs, dependent: :destroy
 
     validates_associated :employee, :vehicle, :customer
 
