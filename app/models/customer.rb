91f7e58e6de7eb90b1dcd58dbe4c1d4c8e7e0996
Cody <cody.frank30@gmail.com>
2019-10-27 21:27:00 -0400

    add password validations to customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,9 +4,12 @@ class Customer < ApplicationRecord
     has_many :jobs, through: :repair_orders
     has_many :employees, through: :repair_orders
 
+    has_secure_password
+
 
     validates :name, presence: true
     validates :email, :phone_number, uniqueness: true, allow_blank: true
     validates :phone_number, length: {is: 10}, allow_blank: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password_confirmation, presence: true, on: :create
 end
