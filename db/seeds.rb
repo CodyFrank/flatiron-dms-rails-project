b60aab92423e72bbd88f14e26382042083fd8a4e
Cody <cody.frank30@gmail.com>
2019-10-31 21:31:25 -0400

    creates and populates seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+cust = Customer.create(name: "John", phone_number: 7775556633, email: "testing@1212.com", password: "password", password_confirmation: "password")
+
+admin = Employee.create(name: "Jane", job_title: "Tester", admin: true, password: "password", password_confirmation: "password", worker_number: 1234)
+emp = Employee.create(name: "doe", job_title: "Driver", admin: false, password: "password", password_confirmation: "password", worker_number: 5678)
+
+veh = cust.vehicles.build(make: "Toyota", model: "Supra", miles: 100000, vin: "12345678910111213")
+
+ro = admin.repair_orders.build(vehicle: veh, finished: false, customer: cust)
+
+ro.jobs.build(concern: "oil change", completed: false)
