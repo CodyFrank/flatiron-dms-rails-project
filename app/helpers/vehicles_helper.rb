f2b12fb6326824c3f0fc037c46f0a392c7d89b20
Cody <cody.frank30@gmail.com>
2019-11-04 19:12:03 -0500

    adds datalist feature to vehicles view

--- app/helpers/vehicles_helper.rb
+++ app/helpers/vehicles_helper.rb
@@ -1,2 +1,10 @@
 module VehiclesHelper
+    
+    def make_collection
+        collection = []
+        Vehicle.all.each do |veh|
+            collection << veh.make
+        end
+        collection.uniq
+    end
 end
